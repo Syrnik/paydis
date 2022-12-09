a4b701ba1856bd395455dc9997ae37593f720b0b
Serge Rodovnichenko <sergerod@gmail.com>
2022-12-10 01:07:45 +0300

    Добавил файл с минимальными требованиями

---
+++ lib/config/requirements.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @copyright Serge Rodovnichenko, 2022
+ */
+return [
+    'php'      => ['version' => '>=7.2.0', 'strict' => true],
+    'app.shop' => ['version' => '>=8.6.0', 'strict' => true],
+];
