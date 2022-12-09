56e95b749a19a5b190f136b99a89529bf9479db8
Serge Rodovnichenko <sergerod@gmail.com>
2022-12-10 01:09:03 +0300

    Заготовка настроек в бэкенде

---
+++ lib/actions/backend/shopPaydisPluginBackend.action.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @copyright Serge Rodovnichenko, 2022
+ * @license Webasyst
+ */
+
+declare(strict_types=1);
+
+/**
+ * @ControllerAction backend/default
+ */
+class shopPaydisPluginBackendAction extends waViewAction
+{
+    public function execute()
+    {
+        $payment_methods = (new shopPluginModel)->listPlugins(shopPluginModel::TYPE_PAYMENT);
+        $payment_methods = array_column($payment_methods, null, 'id');
+
+        $settings = wa('shop')->getPlugin('paydis')->getSettings();
+        $discounts = (array)($settings['discounts'] ?? []);
+        $discounts = array_column($discounts, null, 'payment_id');
+
+        // Добавим отсутствующие, новые способы оплаты
+        foreach ($payment_methods as $method) {
+            if (!isset($discounts[$method['id']]))
+                $discounts[$method['id']] = ['payment_id' => $method['id'], 'discount' => ''];
+        }
+
+        // Вычистим удалённые
+        $discounts = array_filter($discounts, function ($d) use ($payment_methods) {
+            return isset($payment_methods[$d['payment_id']]);
+        });
+
+        $settings['discounts'] = $discounts;
+
+        $this->view->assign(compact('payment_methods', 'settings'));
+    }
+}
